f20d09f4ef488c71f5291bfd8a149e23b8c8255d
fida-ur-rehman <55063076+fida-ur-rehman@users.noreply.github.com>
2022-06-10 11:45:54 +0530

    TEST

--- src/routes/standup.js
+++ src/routes/standup.js
@@ -13,7 +13,7 @@ router.post("/removeMember", isAuthorized, standupController.removeMember);
 router.post("/addMembers", isAuthorized, standupController.addMembers);
 router.post("/remind", isAuthorized, standupController.remindPending);
 router.post("/delete", isAuthorized, standupController.delete);
-router.get("/statusPerOccurrence", isAuthorized, standupController.statusPerOccurence);
+router.post("/statusPerOccurrence", isAuthorized, standupController.statusPerOccurence);
 router.get("/effTable", isAuthorized, standupController.efficiencyNSubmission);
 
 
